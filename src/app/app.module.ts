878f5386b5d75b3d36f2461b240ea5e93fd46d3d
Michael Vargas S <michaelvargas47@hotmail.com>
2023-09-22 17:50:50 -0600

    grid en el componente y funciona el carrucel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponenteProductoComponent } from './components/componente-producto/componente-producto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponenteProductoComponent
   ],
   imports: [
     BrowserModule,
